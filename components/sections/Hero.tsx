711cd81ef1800106f12396a370a21524bc7194b0
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-06-28 03:00:42 +0530

    gap added

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -8,7 +8,7 @@ import { slideUp } from "@/lib/animation";
 
 export default function Hero() {
   return (
-    <div className="bg-gradient-to-b from-[#000319] via-[#2C0368]  to-[#000319] min-h-screen relative  blurred-bottom-border  w-full">
+    <div className="bg-gradient-to-b from-[#000319] via-[#2C0368]  to-[#000319] min-h-screen relative -z-10   w-full">
       <div className="xl:max-w-[1200px] md:max-w-xl mx-auto max-w-[350px] pt-36">
         <div className="absolute inset-0">
           <SparklesCore
@@ -17,7 +17,7 @@ export default function Hero() {
             background="transparent"
             minSize={0.3}
             maxSize={0.6}
-            particleDensity={30}
+            particleDensity={10}
             className="w-full h-full"
             particleColor="#FFFFFF"
           />
