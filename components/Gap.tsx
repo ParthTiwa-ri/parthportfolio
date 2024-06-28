5133591b37059f11bf4f2f69dffc633c689c0ee3
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-06-28 12:02:17 +0530

    gap length increased

--- components/Gap.tsx
+++ components/Gap.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Gap() {
   return (
-    <div className="w-full h-20 bg-gradient-to-b from-[#000318] to-[#161618] bg-red-700"></div>
+    <div className="w-full h-96 bg-gradient-to-b from-[#000318] to-[#161618] bg-red-700"></div>
   );
 }
--- components/MagicBand.tsx
+++ components/MagicBand.tsx
@@ -4,10 +4,10 @@ export default function MagicBand() {
   return (
     <div className="w-full">
       <div className="flex relative z-10 justify-center w-full">
-        <div className=" md:basis-[400px] basis-[350px] h-[35px]  md:h-[56px] wand-shaft">
+        <div className=" md:basis-[400px] basis-[350px] h-[45px]  md:h-[56px] wand-shaft">
           <div className="wand-streak w-full h-[4px]"></div>
         </div>
-        <div className="md:h-[56px] wand-tip h-[35] basis-[75px]  md:basis-[110px]"></div>
+        <div className="md:h-[56px] wand-tip h-[45] basis-[75px]  md:basis-[110px]"></div>
       </div>
     </div>
   );
