7d9134b00bd91931b370742aed3bde4d8185576b
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-06-28 12:35:19 +0530

    hero

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className="bg-gradient-to-b from-[#000319] via-[#2C0368]  to-[#000319] min-h-screen relative -z-10   w-full">
       <div className="xl:max-w-[1200px] md:max-w-xl mx-auto max-w-[350px] pt-36">
-        <div className="absolute inset-0">
+        {/* <div className="absolute inset-0">
           <SparklesCore
             speed={0.01}
             id="tsparticlesfullpage"
@@ -16,7 +16,7 @@ export default function Hero() {
             className="w-full h-full"
             particleColor="#FFFFFF"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
