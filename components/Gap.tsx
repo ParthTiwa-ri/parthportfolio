d7a135d13c99e9a60e063191f3b1563a65e8502f
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-06-28 13:20:17 +0530

    again trying

--- components/Gap.tsx
+++ components/Gap.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Gap() {
   return (
-    <div className="w-full h-96 -mb-80 bg-gradient-to-b from-[#000318] to-[#161618] bg-red-700"></div>
+    <div className="w-full relative -z-10  h-96 -mb-80 bg-gradient-to-b from-[#000318] to-[#161618] bg-red-700"></div>
   );
 }
