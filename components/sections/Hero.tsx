ae0bc96fe4f864c7e8658e44fd86dd81e9cb7208
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-06-28 12:31:24 +0530

    hero

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Hero() {
-  return <div>hello</div>;
+  return   <div className="bg-gradient-to-b from-[#000319] via-[#2C0368]  to-[#000319] min-h-screen relative -z-10   w-full"></div>;
 }
