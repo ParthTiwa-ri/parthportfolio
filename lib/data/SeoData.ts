3268b484633bbdba2f562161e645242fce26a155
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-06-29 18:17:52 +0530

    metadata

---
+++ lib/data/SeoData.ts
@@ -0,0 +1,20 @@
+export const seoData = {
+  title: "Parth Tiwari | Full-End Developer",
+  description:
+    "I' m Parth, a full-stack developer with a passion for frontend and problem-solving. Based in India, I specialize in NextJs, React, Node.js, and product design.",
+  author: "Parth Tiwari",
+
+  url: "https://parthtiwari.in/",
+  keywords: [
+    "Parth Tiwari",
+    "Full-End Developer",
+    "Full Stack Developer",
+    "Frontend Developer",
+    "React Developer",
+    "Next.js Developer",
+    "Node.js Developer",
+    "Parth Tiwari Portfolio",
+    "Parth Tiwari Resume",
+    "Parth Tiwari Projects",
+  ],
+};
