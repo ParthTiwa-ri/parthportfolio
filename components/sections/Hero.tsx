d49434964a5dbfb8f09940bc44347e368c2e870d
Parth Tiwari <152166714+ParthTiwa-ri@users.noreply.github.com>
2024-07-06 18:09:43 +0530

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
         <div className="flex animate-slide-up justify-center relative my-20 z-10">
           <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
             <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-              Dynamic Web Magic with Next.js
+              Interactive Web Wizardry with Next.js
             </h2>
             <div>
               <div
